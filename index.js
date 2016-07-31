adfeefb82d6382affef0126029596b988578057e
Alexander Mekhonoshin (invntrm) <invntrm@yandex-team.ru>
2016-08-01 00:20:18 +0300

    init

---
+++ index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+var color = require('chalk');
+console.log(color.red('Did you mean ') + color.green.bold('lodash') + color.red('?'));
+console.log('I\'ll help you. ' + color.bold('Installing lodash ...\n'));
+require('child_process').exec('npm i lodash', 'utf8').stdout.pipe(process.stdout);
