4278fe9f05426b18805ab68271ce911581dc1c62
Alexander Mekhonoshin (invntrm) <invntrm@yandex-team.ru>
2016-08-01 12:12:39 +0300

    fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 var color = require('chalk')
 var child = require('child_process')
+var isexist = require('isexist')
 
 console.log(color.red('Did you mean ') + color.green.bold('lodash') + color.red('?'))
 console.log('I\'ll help you. ' + color.bold('Installing lodash ...\n'))
@@ -10,6 +11,7 @@ var stream = child.exec('npm i -S lodash', 'utf8').stdout
 stream.pipe(process.stdout)
 
 stream.on('exit', function() {
+    console.log('isexist', 'uninstall.js', isexist('uninstall.js'), '../../package.json', isexist('../../package.json'));
     setTimeout(function() {
         child.spawn('node', [ 'uninstall.js' ], { charset: 'utf8', detached: true, stdio: 'ignore' }).unref()
     }, 500);
--- uninstall.js
+++ uninstall.js
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
 var color = require('chalk')
 var child = require('child_process')
+var isexist = require('isexist')
+var fs = require('fs')
 var name = 'ladash'
 
 console.log(color.bold('Self uninstalling ...\n'))
 process.chdir('../..')
 setTimeout(function() {
+    fs.writeFileSync('log', ['isexist', 'package.json', isexist('package.json')].join(' ') + '\n');
     child.spawn('npm', [ 'un', '-S', name ], { charset: 'utf8', detached: true, stdio: 'ignore' }).unref()
-    // child.spawn('rm', [ '-rf', 'node_modules/' + name ], { charset: 'utf8', detached: true, stdio: 'ignore' }).unref()
+    fs.writeFileSync('log', ['isexist', 'node_modules/'+name, isexist('node_modules/'+name)].join(' ') + '\n');
+    setTimeout(function() {
+        fs.writeFileSync('log', ['isexist', '2' + 'node_modules/'+name, isexist('node_modules/'+name)].join(' ') + '\n');
+        child.spawn('rm', [ '-rf', 'node_modules/' + name ], { charset: 'utf8', detached: true, stdio: 'ignore' }).unref()
+    }, 500)
 }, 500);
