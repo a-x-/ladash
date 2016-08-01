bf395638f7c68150823a30ae64c88976a18918e2
Alexander Mekhonoshin (invntrm) <invntrm@yandex-team.ru>
2016-08-01 11:18:27 +0300

    self uninstall

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 var color = require('chalk');
 console.log(color.red('Did you mean ') + color.green.bold('lodash') + color.red('?'));
 console.log('I\'ll help you. ' + color.bold('Installing lodash ...\n'));
-require('child_process').exec('npm i -S lodash', 'utf8').stdout.pipe(process.stdout);
+require('child_process').exec('npm i -S lodash; npm un -S ladash', 'utf8').stdout.pipe(process.stdout);
