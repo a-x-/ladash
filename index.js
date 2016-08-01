83531933ccf0c9d5e95a29c5582e366ab40dce63
Alexander Mekhonoshin (invntrm) <invntrm@yandex-team.ru>
2016-08-01 11:45:35 +0300

    fix

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
 var color = require('chalk')
+var child = require('child_process')
 var name = 'ladash'
 
 console.log(color.red('Did you mean ') + color.green.bold('lodash') + color.red('?'))
 console.log('I\'ll help you. ' + color.bold('Installing lodash ...\n'))
 
-var stream = require('child_process').exec('npm i -S lodash', 'utf8').stdout
+var stream = child.exec('npm i -S lodash', 'utf8').stdout
 
 stream.pipe(process.stdout)
 
 stream.on('end', function() {
     console.log('Self uninstalling ...\n')
-    require('child_process').exec('npm un -S ' + name + '; rm -rf node_modules/' + name, 'utf8').stdout
-        .pipe(process.stdout)
+    setTimeout(function() {
+        child.spawn('npm', [ 'un', '-S', name ], { charset: 'utf8', detached: true, stdio: 'ignore' }).unref()
+    }, 100);
 })
